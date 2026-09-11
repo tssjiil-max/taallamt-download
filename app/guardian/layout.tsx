4b064e7e9eb471e5812e788a92db3da1307b89d7
tssjiil-max <tssjiil@gmail.com>
2026-09-11 11:56:43 +0300

    Enable direct guardian links in student layout

--- app/guardian/layout.tsx
+++ app/guardian/layout.tsx
@@ -1,7 +1,8 @@
+import { DirectGuardianLogin } from "@/components/DirectGuardianLogin";
 import { StudentFrame } from "@/components/StudentChrome";
 import "./guardian-polish.css";
 import "../student-unified.css";
 
 export default function GuardianLayout({children}:{children:React.ReactNode}){
-  return <StudentFrame>{children}</StudentFrame>;
+  return <StudentFrame><DirectGuardianLogin />{children}</StudentFrame>;
 }
