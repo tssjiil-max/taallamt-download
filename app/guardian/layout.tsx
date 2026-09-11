e668f9e1decba9a8efe44e16561fe6d8efc95b3b
tssjiil-max <tssjiil@gmail.com>
2026-09-11 16:10:41 +0300

    Load new student Shakabumbo icon set

--- app/guardian/layout.tsx
+++ app/guardian/layout.tsx
@@ -1,5 +1,6 @@
 import "./guardian-polish.css";
 import "../student-unified.css";
+import "./student-icons.css";
 
 export default function GuardianLayout({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
