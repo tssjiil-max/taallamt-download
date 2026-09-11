01da18ec4844858869aa59af6886f8c8eec87071
tssjiil-max <tssjiil@gmail.com>
2026-09-11 12:03:53 +0300

    Use single direct-link handler in student frame

--- app/guardian/layout.tsx
+++ app/guardian/layout.tsx
@@ -1,8 +1,7 @@
-import { DirectGuardianLogin } from "@/components/DirectGuardianLogin";
 import { StudentFrame } from "@/components/StudentChrome";
 import "./guardian-polish.css";
 import "../student-unified.css";
 
 export default function GuardianLayout({children}:{children:React.ReactNode}){
-  return <StudentFrame><DirectGuardianLogin />{children}</StudentFrame>;
+  return <StudentFrame>{children}</StudentFrame>;
 }
